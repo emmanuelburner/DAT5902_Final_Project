4190bb1a0236b1a63f7ffa2c29545c811fc90dba
RammusOTP <emmanuelphillips33@gmail.com>
2025-01-02 12:53:20 +0000

    Imports and dataframe creation

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
-print("Hello world")
+import math
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+# Change base pandas settings to view all rows and cols when using .head()
+pd.set_option('display.max_rows', None)
+pd.set_option('display.max_columns', None)
+
+# Read the csv 
+df = pd.read_csv(r'data/LeagueofLegends.zip')
+print(df.head(1)) # Check the dataframe is correct
